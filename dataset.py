c2c95f142ff7fb623bacce545a963860246e03f7
garylab2030 <plusoneee@smail.nchu.eud.tw>
2022-02-25 11:24:13 +0800

    add xlnet

--- dataset.py
+++ dataset.py
@@ -1,18 +1,14 @@
-import numpy as np
 import pandas as pd
-from transformers import BertTokenizer
 import torch
-from torch.utils.data.sampler import WeightedRandomSampler
+from transformers import BertTokenizer, XLNetTokenizer
 from torch.utils.data import DataLoader, Dataset
+from torch.utils.data.sampler import WeightedRandomSampler
 
 class DatasetUtils:
-
     @staticmethod
     def get_loader(dataset, batch_size=32):
         sample_weights = [0] * len(dataset)
-
         class_weights = dataset.get_class_weights()
-
         for idx, data in enumerate(dataset):
             label = data['targets'].int()
             class_weight = class_weights[label]
@@ -27,19 +23,39 @@ class DatasetUtils:
         return loader
 
     @staticmethod
-    def get_tokenizer(name='bert-base-uncased'):
-        tokenizer = BertTokenizer.from_pretrained(name)
+    def get_tokenizer(name='xlnet'):
+        assert name.lower() in ['bert', 'xlnet']
+
+        print(f"===== Create {name.upper()} Tokenizer =====")
+        if name == 'bert':
+            tokenizer = BertTokenizer.from_pretrained(
+                'bert-base-uncased'
+            )
+        elif name == 'xlnet':
+            tokenizer = XLNetTokenizer.from_pretrained(
+                'xlnet-base-cased',
+                do_lower_case = True,
+                remove_space= True
+                )
+
         return tokenizer
 
 class MoodyLyrics(Dataset):
-    def __init__(self, filepath, tokenizer, max_len=128, mode='train'):
+    def __init__(self, root_dir, tokenizer, max_len=128, mode='train'):
         assert mode in ['train', 'val']
-
-        self.df = pd.read_csv(filepath)
-        self.tokenizer = tokenizer
-        self.max_len = max_len
+        
         self.mode = mode
 
+        if self.mode == 'train':
+            self.df = pd.read_csv(f'{root_dir}/Train.csv')
+            print('* Train size:', len(self.df))
+        else: 
+            self.df = pd.read_csv(f'{root_dir}/Test.csv')
+            print('* Validation size:', len(self.df))
+
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        
         # label to index
         self.label_map = {
             'happy': 0,
@@ -48,17 +64,6 @@ class MoodyLyrics(Dataset):
             'relaxed': 3
         }
 
-        self.len = len(self.df)
-        self.train_len = int(self.len * 0.8)
-        
-        if self.mode == 'train':
-            self.df = self.df[: self.train_len]
-            print('Train size:', len(self.df))
-        else: 
-            self.df = self.df[self.train_len:]
-            print('Validation size:', len(self.df))
-            print(self.df)
-
     def label_value_counts(self):
         return self.df.mood.value_counts()
 
@@ -66,19 +71,16 @@ class MoodyLyrics(Dataset):
         label_vc = self.label_value_counts()
         # happy, angry, sad, relaxed 4 classes
         _class_weights = [0] * len(label_vc) 
-
         for labelname, _count in label_vc.items():
             classindex = self.label_map[labelname]
-            _class_weights[classindex] = 100/_count
+            _class_weights[classindex] = 10000/_count
         return _class_weights
-
+        
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, idx):
-        
-        lyric = self.df.lyric.replace(r'(<.*\/>)', '').iloc[idx]
-        
+        lyric = self.df.lyric.iloc[idx]
         _label_str = self.df.mood.iloc[idx]
         label = self.label_map[_label_str]
 
@@ -99,30 +101,27 @@ class MoodyLyrics(Dataset):
             'ids': torch.tensor(ids, dtype=torch.long),
             'mask': torch.tensor(mask, dtype=torch.long),
             'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
-            'targets': torch.tensor(label, dtype=torch.float)
+            'targets': torch.tensor(label, dtype=torch.long)
         }
 
 if __name__ == '__main__':
-    
-    tokenizer = DatasetUtils.get_tokenizer()
+
+    # BERT example
+    tokenizer = DatasetUtils.get_tokenizer('bert')
     dataset = MoodyLyrics(
-        filepath='MoodylyricDataset/SegmentMoodyLyrics4Q.csv',
+        root_dir='MoodylyricDataset',
         tokenizer=tokenizer,
-        mode='train'
+        mode='train',
+        max_len=256
     )
-    train_loader = DatasetUtils.get_loader(
-        dataset,
-        32
+    train_loader = DatasetUtils.get_loader(dataset, 1)
+
+    # XLNET example
+    tokenizer = DatasetUtils.get_tokenizer('xlnet')
+    dataset = MoodyLyrics(
+        root_dir='MoodylyricDataset',
+        tokenizer=tokenizer,
+        mode='train',
+        max_len=1024
     )
-
-
-    '''
-    happy, angry, sad, relaxed = 0, 0, 0, 0
-    for data in train_loader:
-        labels = data['targets'].int()
-        happy += torch.sum(labels==0)
-        angry += torch.sum(labels==1)
-        sad += torch.sum(labels==2)
-        relaxed += torch.sum(labels==3)
-    print(happy, angry, sad, relaxed)
-    '''
+    train_loader = DatasetUtils.get_loader(dataset, 1)
